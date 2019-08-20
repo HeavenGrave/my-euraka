353dc95e200fa78a1d8c633220c7a96344a7d3b6
HeavenGrave <285091610@qq.com>
2019-08-20 21:09:17 +0800

    Initial commit

---
+++ my-euraka-demo/src/main/java/com/yjg/myeurakademo/MyEurakaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yjg.myeurakademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEurakaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEurakaDemoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yjg/myeuraka/MyEurakaApplication.java
@@ -0,0 +1,14 @@
+package com.yjg.myeuraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MyEurakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEurakaApplication.class, args);
+    }
+
+}
